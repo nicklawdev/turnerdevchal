575853c879d527d558b0188d4d03fcb1fa12ab95
nicklawdev <nicklawrenceemail@gmail.com>
2019-10-08 11:54:55 -0400

    adding details modal

--- routes/api/titles.js
+++ routes/api/titles.js
@@ -8,14 +8,20 @@ router.route('/')
         Title.find({}, (err, titles) => {
             res.json(titles);
         })  
-})
+});
 
 router.route('/:titleName')
     .get((req, res) => {
         Title.find({TitleName: req.params.titleName}, (err, titles) => {
-            console.log(titles);
             res.json(titles);
         })  
-})
+});
+
+router.route('/id/:titleId')
+    .get((req, res) => {
+        Title.find({TitleId: req.params.titleId}, (err, titles) => {
+            res.json(titles);
+        })  
+});
 
 module.exports = router;
